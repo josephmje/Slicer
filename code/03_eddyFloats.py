8ab1ca223c4709095e3f60e6c37ff98f0cb65eac
Michael Joseph <josephmje.22@gmail.com>
2020-04-08 14:54:03 -0400

    update float to short script

--- code/03_eddyFloats.py
+++ code/03_eddyFloats.py
@@ -1,50 +1,24 @@
 #!/usr/bin/env python
-import nibabel as nib
+import nibabel as nb
 import numpy as np
-import pandas as pd
 import os
 from glob import glob
 
 #We need to convert all of the eddy-corrected images to 'ints', and remove the decimal place
 #The reason for this is that dtiprep doesn't allow for decimal places that eddy produces???
-#If running in the terminal, module load Python 3+, and then run in ipython
-#(If getting errors about indents, may be because Python doesn't like spaces in the terminal)
 
-#Note to self: because of difficulties installing dependencies in my own Python environement, I will use the lab's Python to run the below
-#This code takes a bit of time to run -- but <1 hour
+input_dir   = "/scratch/mjoseph/bids/STOPPD/derivatives/tractography/eddy"
+output_dir  = "/scratch/mjoseph/bids/STOPPD/derivatives/tractography/float"
 
-#module load python/3.8.1
-#ipython
+eddy_files = sorted(glob(f"{input_dir}/*nii.gz"))
+for f in eddy_files:
 
-#make the directory for the output
-#mkdir /projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT
-#this takes about 6 seconds per participant -- so about an hour for SPINS if running on computer
+    base_file = os.path.basename(f).replace("_desc-preproc_dwi.nii.gz", "")
+    output_file = os.path.join(output_dir, base_file + "_eddy_fixed.nii.gz")
 
-#note to self: for some reason, this didn't respect the sublist -- it included participants (e.g.. human phantoms), who were on that list (ignoring that problem for now; will remove later).
-
-input_dir   = "/projects/ncalarco/thesis/SPINS/Slicer/data/01_dmriprep"
-output_dir  = "/projects/ncalarco/thesis/SPINS/Slicer/data/03_dmriprep_INT"
-sublist = open("/projects/ncalarco/thesis/SPINS/Slicer/txt_outputs/03_sublist.txt", "r")
-SID = sublist.readlines()
-SID = [i.replace('\n', '') for i in SID]
-
-for i in SID:
-    print(i)
-    check_exists = os.path.join(output_dir, i, i + '_eddy_fixed.nii.gz')
-
-    if not os.path.isfile(check_exists):
-        os.makedirs(os.path.join(output_dir, i))
-        sub_image = glob(os.path.join(input_dir, i, '*', '*', '*desc-brainsuite_dwi.nii.gz'))[0]
-
-    if os.path.isfile(sub_image):
-        img = nib.load(sub_image)
-        hdr = img.header
-        new_data = np.copy(img.get_data())
-        new_dtype = np.int16
-        new_data = new_data.astype(new_dtype)
-        img.set_data_dtype(new_dtype)
-
-        new_image = nib.Nifti1Image(new_data, img.affine, header=hdr)
-        print(new_image.get_data_dtype())
-
-        nib.save(new_image, check_exists)
+    if not os.path.isfile(output_file):
+        img = nb.load(f)
+        data = img.get_fdata().astype(np.int16)
+        new_img = nb.Nifti1Image(data, img.affine, img.header)
+        new_img.header.set_data_dtype(np.int16)
+        new_img.to_filename(output_file)
